0036f54a8cd4c9004187e35f9ba39b63887d0ba2
João Luis <joaoluislopesaraujo088@gmail.com>
2025-05-24 17:19:28 -0300

    Exercicio 8 - Calcular desconto em alcool ou gaso

---
+++ exercicio 8.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    float litros;
+    char combustivel;
+
+    printf("\nDigite com a letra correspondente:\n A - Álcool ou G - Gasolina. \n");
+    scanf("%c", &combustivel);
+    printf("Você abasteceu quantos litros? ");
+    scanf("%f", &litros);
+
+    if (combustivel == 'A'){
+        if (litros <= 20){
+            printf("Você tem desconto no Álcool! Ficou: R$%.2f\n", (litros*2.90)*0.97);
+        }
+        else{
+            printf("Você tem desconto no Álcool! Ficou: R$%.2f\n", (litros*2.90)*0.95);
+        }
+    }
+    else{
+        if (litros <= 20){
+            printf("Você tem desconto na Gasolina! Ficou: R$%.2f\n", (litros*3.30)*0.96);
+        }
+        else{
+            printf("Você tem desconto na Gasolina! Ficou: R$%.2f\n", (litros*3.30)*0.94);
+        }
+        
+    }
+    
+    system("pause");
+    return 0;
+}
