3f9bbc77d7360f609e264501dcf658cec3472818
João Luis <joaoluislopesaraujo088@gmail.com>
2025-05-25 13:22:00 -0300

    Calculando peso ideal F ou M
    Bom exercício, descobri como comparar dois resultados do tipo char na estrutura.

---
+++ exercicio 9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*leia a altura (h) e o sexo (M / F) de uma pessoa, calcule e imprima o
+peso ideal*/
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    float altura, ideal;
+    char sexo;
+
+    printf("Qual seu sexo?\nM - Masculino\nF - Feminino\n");
+    scanf("%c", &sexo);
+    printf("Qual sua altura em metros? ");
+    scanf("%f", &altura);
+
+    if (sexo == 'M' || sexo == 'm'){
+        ideal = (72.7*altura)-58;
+        printf("Seu peso ideal M é de: %.2f kg\n", ideal);
+
+    }
+    else{
+        ideal = (62.1*altura)-44.7;
+        printf("Seu peso ideal F é de: %.2f kg\n", ideal);
+    }
+
+    system("pause");
+    return 0;
+}
